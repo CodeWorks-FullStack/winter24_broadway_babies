1e8aeb1a23b8cd31fc6d6bfa21fdd4efaaf27121
Jeremy Fowler <jembyfowler@gmail.com>
2024-11-20 11:45:01 -0700

    creating show and populating virtuals after create

--- src/controllers/ShowsController.js
+++ src/controllers/ShowsController.js
@@ -6,6 +6,7 @@ export class ShowsController extends BaseController {
     super('api/shows')
     this.router
       .get('', this.getAllShows)
+      .post('', this.createShow)
   }
   /**
     * Returns all shows
@@ -17,6 +18,22 @@ export class ShowsController extends BaseController {
     try {
       const shows = await showsService.getAllShows()
       response.send(shows)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+    * Create a show for a handler and an animal
+    * @param {import("express").Request} request
+    * @param {import("express").Response} response
+    * @param {import("express").NextFunction} next
+    */
+  async createShow(request, response, next) {
+    try {
+      const showData = request.body
+      const show = await showsService.createShow(showData)
+      response.send(show)
     } catch (error) {
       next(error)
     }
--- src/services/ShowsService.js
+++ src/services/ShowsService.js
@@ -1,6 +1,13 @@
 import { dbContext } from "../db/DbContext.js"
 
 class ShowsService {
+  async createShow(showData) {
+    const show = await dbContext.Shows.create(showData)
+    // NOTE populates must be done after a create
+    await show.populate('animal')
+    await show.populate('handler', 'name picture')
+    return show
+  }
   async getAllShows() {
     const shows = await dbContext.Shows.find().populate('animal').populate('handler', 'name picture')
     return shows
