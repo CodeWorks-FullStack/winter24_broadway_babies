1737851936b3a23e9aece16968e7cc1971721314
Jeremy Fowler <jembyfowler@gmail.com>
2024-11-20 10:52:32 -0700

    added shows collection to dbcontext

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { AnimalSchema } from '../models/Animal.js';
+import { ShowSchema } from '../models/Show.js';
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
-  Animals = mongoose.model('Animal', AnimalSchema)
+  Animals = mongoose.model('Animal', AnimalSchema);
+  Shows = mongoose.model('Show', ShowSchema);
 }
 
 export const dbContext = new DbContext()
