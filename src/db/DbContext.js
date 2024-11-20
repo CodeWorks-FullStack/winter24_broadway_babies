1e713a4e9b7fe01d4d2fd2e43a194ceedfa8964c
Jeremy Fowler <jembyfowler@gmail.com>
2024-11-20 09:42:27 -0700

    added collection to dbcontext

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { AnimalSchema } from '../models/Animal.js';
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
+  Animals = mongoose.model('Animal', AnimalSchema)
 }
 
 export const dbContext = new DbContext()
